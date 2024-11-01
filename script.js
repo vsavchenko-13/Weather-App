2da56aa879004067efd6d3171d26571097e0d5be
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-31 19:31:09 -0600

    added very basic api functionality

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const city = document.querySelector("#city");
 const form = document.querySelector("form");
 form.addEventListener("submit", getWeather);
 
-async function getWeather(){
+async function getWeather(event){
     event.preventDefault();
-    const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?key=FV4AGXK25ZGCPGKDF3MVNY5D6 "
-    const response = await fetch(url,{
+    const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city.value + "?key=FV4AGXK25ZGCPGKDF3MVNY5D6&contentType=json"
+
+    const request = new Request(url,{
         mode: "cors"
     });
+
+    const response = await fetch(request);
     if(response.ok){
-        console.log(response);
+        const data = await response.json();
+        console.log(data.currentConditions.temp)
     } else {
         console.log(response);
     }
+    
 }
